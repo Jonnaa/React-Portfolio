465a6cc3c54bbd284688124a80a14d842ce85077
Jonathan <Jonnaa96@yahoo.com>
2023-11-29 18:16:01 -0700

    Light icons show first | Added resume button that links to resume on google docs

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -8,8 +8,17 @@ export default function ContactMe(){
                     <img src="icons/github-logo.png" alt="dark github logo" id="darkGit" />
                     <img src="icons/github-mark-white.png" alt="light github logo" id="lightGit" />
                 </a>
+
+                {/* Resume Link */}
+                <a href="https://docs.google.com/document/d/1Ax3boPILXauHVPWhFwN68AZ32XCmoPduiGOSoLN0tVo/edit?usp=sharing" rel="noreferrer" target="_blank" id="resume">
+                    <button className="resumeBtn">
+                        Resume
+                    </button>
+                </a>
+                
+
                 {/* LinkedIn Link */}
-                <a href="https://www.linkedin.com/in/jonathannavarroswe/" id="linkedIn">
+                <a href="https://www.linkedin.com/in/jonathannavarroswe/" rel="noreferrer" target="_blank" id="linkedIn">
                     <img src="icons/linkedInLogo.png" alt="LinkedIn logo" id="darkLi" />
                     <img src="icons/In-White-128.png" alt="LinkedIn logo" id="lightLi" />
                 </a>
