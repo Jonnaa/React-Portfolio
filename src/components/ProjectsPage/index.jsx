01e5fc707039c3940faddfa739b4863162c2018e
Jonathan <Jonnaa96@yahoo.com>
2023-12-04 16:32:31 -0700

    Added project screenshots that will go in projects page

--- src/components/ProjectsPage/index.jsx
+++ src/components/ProjectsPage/index.jsx
@@ -2,9 +2,29 @@ import "./styles.css"
 
 export default function ProjectsPage(){
 
+    let tempoImg = <>
+        <img src="" alt="" className="projectImg"/>
+    </>
     return (
         <div className="projDiv">
             <h1>Projects Page</h1>
+
+            <div className="projectsDiv">
+                <div className="tempo">
+                    <h2>Tempo</h2>
+                    <figure>
+                        <div className="tempoImgs">
+
+                        </div>
+                    </figure>
+                </div>
+                <div className="kdramas">
+
+                </div>
+                <div className="kit">
+
+                </div>
+            </div>
         </div>
     )
 }
