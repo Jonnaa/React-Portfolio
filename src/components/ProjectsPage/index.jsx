b314cdcb72a6e8a1a8e8b02d7e218101c683c4d0
Jonathan <Jonnaa96@yahoo.com>
2023-12-04 16:45:43 -0700

    Started carousel logic for 1 project

--- src/components/ProjectsPage/index.jsx
+++ src/components/ProjectsPage/index.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function ProjectsPage(){
-
+    const [tempoCount, setTempoCount] = useState(1)
     let tempoImg = <>
-        <img src="" alt="" className="projectImg"/>
+        <img src="tempoScreenshots/Home.jpg" alt="" className="projectImg"/>
     </>
+
+    function handlePrev(event){
+        let projName = event.target.className
+        switch(projName){
+            case 'tempoBtn':
+                console.log("Tempo")
+                break
+            default:
+                console.log("No such project")
+        }
+    }
+
+    function handleNext(event){
+        let projName = event.target.className
+        switch(projName){
+            case 'tempoBtn':
+                console.log("Tempo")
+                break
+            default:
+                console.log("No such project")
+        }
+    }
     return (
         <div className="projDiv">
             <h1>Projects Page</h1>
@@ -12,11 +35,12 @@ export default function ProjectsPage(){
             <div className="projectsDiv">
                 <div className="tempo">
                     <h2>Tempo</h2>
-                    <figure>
-                        <div className="tempoImgs">
+                    <div className="tempoPics">
+                        <button onClick={handlePrev} className="tempoBtn">Prev</button>
+                        {tempoImg}
+                        <button onClick={handleNext} className="tempoBtn">Next</button>
 
-                        </div>
-                    </figure>
+                    </div>
                 </div>
                 <div className="kdramas">
 
