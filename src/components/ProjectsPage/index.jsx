56de024e33a2ff1e13db330c6d316b5f832b56e7
Jonathan <Jonnaa96@yahoo.com>
2023-12-04 17:56:07 -0700

    first project carousel logic complete

--- src/components/ProjectsPage/index.jsx
+++ src/components/ProjectsPage/index.jsx
@@ -4,14 +4,30 @@ import "./styles.css"
 export default function ProjectsPage(){
     const [tempoCount, setTempoCount] = useState(1)
     let tempoImg = <>
-        <img src="tempoScreenshots/Home.jpg" alt="" className="projectImg"/>
+        {/* <img src="tempoScreenshots/Home.jpg" alt="" className="projectImg"/> */}
     </>
 
+    switch(tempoCount){
+        case 1:
+            tempoImg = <><img src="tempoScreenshots/Home.jpg" alt="" className="projectImg"/></>
+            break
+        case 2:
+            tempoImg = <><img src="tempoScreenshots/ArtistIndex.jpg" alt="" className="projectImg"/></>
+            break
+        case 3:
+            tempoImg = <><img src="tempoScreenshots/ArtistDetail.jpg" alt="" className="projectImg"/></>
+            break
+    }
     function handlePrev(event){
         let projName = event.target.className
         switch(projName){
-            case 'tempoBtn':
-                console.log("Tempo")
+            case 'tempoBtn btn':
+                if(tempoCount>=2){
+                    setTempoCount(tempoCount-1)
+                }
+                else{
+                    setTempoCount(3)
+                }
                 break
             default:
                 console.log("No such project")
@@ -20,9 +36,15 @@ export default function ProjectsPage(){
 
     function handleNext(event){
         let projName = event.target.className
+        // console.log(projName)
         switch(projName){
-            case 'tempoBtn':
-                console.log("Tempo")
+            case 'tempoBtn btn':
+                if(tempoCount<=2){
+                    setTempoCount(tempoCount+1)
+                }
+                else{
+                    setTempoCount(1)
+                }
                 break
             default:
                 console.log("No such project")
@@ -34,12 +56,11 @@ export default function ProjectsPage(){
 
             <div className="projectsDiv">
                 <div className="tempo">
-                    <h2>Tempo</h2>
+                    <h2> <a href="https://tempoproject-1ccff4d917c0.herokuapp.com/home/" rel="noreferrer" target="_blank">Tempo</a></h2>
                     <div className="tempoPics">
-                        <button onClick={handlePrev} className="tempoBtn">Prev</button>
+                        <button onClick={handlePrev} className="tempoBtn btn">P<br/>r<br/>e<br/>v</button>
                         {tempoImg}
-                        <button onClick={handleNext} className="tempoBtn">Next</button>
-
+                        <button onClick={handleNext} className="tempoBtn btn">N<br/>e<br/>x<br/>t</button>
                     </div>
                 </div>
                 <div className="kdramas">
