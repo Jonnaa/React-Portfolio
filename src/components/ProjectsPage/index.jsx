e53e17994f385e41caa26f2ba0149591b20c733c
Jonathan <Jonnaa96@yahoo.com>
2023-12-04 18:04:30 -0700

    second project carousel setup

--- src/components/ProjectsPage/index.jsx
+++ src/components/ProjectsPage/index.jsx
@@ -3,9 +3,8 @@ import "./styles.css"
 
 export default function ProjectsPage(){
     const [tempoCount, setTempoCount] = useState(1)
-    let tempoImg = <>
-        {/* <img src="tempoScreenshots/Home.jpg" alt="" className="projectImg"/> */}
-    </>
+    const [kdramasCount, setKdramasCount] = useState(1)
+    let tempoImg = <></>, kdramasImg=<></>
 
     switch(tempoCount){
         case 1:
@@ -16,6 +15,17 @@ export default function ProjectsPage(){
             break
         case 3:
             tempoImg = <><img src="tempoScreenshots/ArtistDetail.jpg" alt="" className="projectImg"/></>
+            break
+    }
+    switch(kdramasCount){
+        case 1:
+            kdramasImg = <><img src="kdramasScreenshots/Home Page.jpg" alt="" className="projectImg"/></>
+            break
+        case 2:
+            kdramasImg = <><img src="kdramasScreenshots/Reviews.jpg" alt="" className="projectImg"/></>
+            break
+        case 3:
+            kdramasImg = <><img src="kdramasScreenshots/Review Details.jpg" alt="" className="projectImg"/></>
             break
     }
     function handlePrev(event){
@@ -29,6 +39,14 @@ export default function ProjectsPage(){
                     setTempoCount(3)
                 }
                 break
+                case 'kdramasBtn btn':
+                    if(kdramasCount>=2){
+                        setKdramasCount(kdramasCount-1)
+                    }
+                    else{
+                        setKdramasCount(3)
+                    }
+                    break
             default:
                 console.log("No such project")
         }
@@ -46,6 +64,14 @@ export default function ProjectsPage(){
                     setTempoCount(1)
                 }
                 break
+            case 'kdramasBtn btn':
+                if(kdramasCount<=2){
+                    setKdramasCount(kdramasCount+1)
+                }
+                else{
+                    setKdramasCount(1)
+                }
+                break
             default:
                 console.log("No such project")
         }
@@ -56,15 +82,20 @@ export default function ProjectsPage(){
 
             <div className="projectsDiv">
                 <div className="tempo">
-                    <h2> <a href="https://tempoproject-1ccff4d917c0.herokuapp.com/home/" rel="noreferrer" target="_blank">Tempo</a></h2>
-                    <div className="tempoPics">
+                <h2> <a href="https://tempoproject-1ccff4d917c0.herokuapp.com/home/" rel="noreferrer" target="_blank">Tempo</a></h2>
+                    <div className="projPics">
                         <button onClick={handlePrev} className="tempoBtn btn">P<br/>r<br/>e<br/>v</button>
                         {tempoImg}
                         <button onClick={handleNext} className="tempoBtn btn">N<br/>e<br/>x<br/>t</button>
                     </div>
                 </div>
                 <div className="kdramas">
-
+                    <h2><a href="https://kdramas-4e345146c0e4.herokuapp.com" rel="noreferrer" target="_blank">K-Dramas</a></h2>
+                    <div className="projPics">
+                        <button onClick={handlePrev} className="kdramasBtn btn">P<br/>r<br/>e<br/>v</button>
+                        {kdramasImg}
+                        <button onClick={handleNext} className="kdramasBtn btn">N<br/>e<br/>x<br/>t</button>
+                    </div>
                 </div>
                 <div className="kit">
 
