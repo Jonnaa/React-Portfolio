eb1f43ead079d9d5d2db497fabd19cf0523035a0
Jonathan <Jonnaa96@yahoo.com>
2023-12-11 17:42:41 -0700

    Added burger navbar under 768px

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,9 +4,14 @@ import AboutMePage from '../AboutMePage';
 import ProjectsPage from '../ProjectsPage';
 import ContactMe from '../ContactMe';
 import { Routes, Route, Link } from "react-router-dom";
+import { useState } from 'react';
 
 export default function App() {
+  const [burgerHidden, setBurgerHidden]= useState(true)
 
+  function toggleBurger(){
+    setBurgerHidden(!burgerHidden)
+  }
   return (
     <div className='mainDiv'>
       
@@ -16,6 +21,19 @@ export default function App() {
         <Link to="/about">About Me</Link>
         <Link to="/projects">Projects</Link>
       </div>
+      {/* Burger nav bar */}
+      <div className='navBarBurger'>
+        <button onClick={toggleBurger}><img src="icons/burger-bar.png" alt="" className='burger'/></button>
+      </div>
+      {burgerHidden?<></>:
+        <>
+          <div className="burgerLinks">
+            <Link to="/" onClick={toggleBurger} className='burgerLink homeLink'>Home Page</Link>
+            <Link to="/about" onClick={toggleBurger} className='burgerLink aboutLink'>About Me</Link>
+            <Link to="/projects" onClick={toggleBurger} className='burgerLink'>Projects</Link>
+          </div>
+        </>
+      }
       
       {/* "Pages" div */}
       <div className='routeDiv'>
